5b3ece2f17281a7ac8dbd8fd6843dd83ffaad6fa
jagasri3 <jagasrimvl@gmail.com>
2026-09-13 19:47:11 +0530

    Format Terraform provider configuration

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-backend "s3" {
-  bucket       = "terraform-project03-state-851725554910"
-  key          = "project03/terraform.tfstate"
-  region       = "ap-south-1"
-  use_lockfile = true
-}
+  backend "s3" {
+    bucket       = "terraform-project03-state-851725554910"
+    key          = "project03/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
